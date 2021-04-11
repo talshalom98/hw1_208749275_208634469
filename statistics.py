9def09dd7984a0b385f0299f1f199b9b2f91b903
talshalom98 <81562877+talshalom98@users.noreply.github.com>
2021-04-11 14:58:19 +0300

    Create statistics.py

---
+++ statistics.py
@@ -0,0 +1,15 @@
+def sum(values):
+  sum=0
+  for i int values:
+    sum=sum+i
+  return sum
+
+
+def mean(values):
+  number_of_elem=len(values)
+  sum=0
+  mean=0
+  for i int values:
+    sum=sum+i
+  mean=sum/number_of_elem
+  return mean
